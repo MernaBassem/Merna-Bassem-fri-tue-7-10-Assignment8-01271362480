1c912d8bc0c89e73ef4c962ec98842a2e170238d
Merna Bassem <mernabassem21@gmail.com>
2024-07-02 15:03:35 +0300

    iconnection db use mongoose

---
+++ DB/connection.js
@@ -0,0 +1,12 @@
+// connection file with mongoose
+
+import mongoose from "mongoose";
+
+export const connection_db =  async ()=>{
+    try{
+       await mongoose.connect("mongodb://localhost:27017/book_store")
+    }
+    catch(error){
+        console.log("Error connecting to the database",error)
+    }
+}
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // index.js file
 import express from "express";
+import { connection_db } from "./DB/connection.js";
 
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+connection_db
 
 app.get("/", (req, res) => res.send("Hello World"));
 
